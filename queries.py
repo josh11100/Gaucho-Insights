64550bbc987d5cb0c8ca4b1f45c46f1b17440cf6
Joshua Chung <152940247+josh11100@users.noreply.github.com>
2026-02-23 13:02:31 -0800

    Update queries.py

--- queries.py
+++ queries.py
@@ -1,8 +1,14 @@
-# queries.py
-
-# Query using actual SQLite syntax
 GET_RECENT_LECTURES = """
 SELECT * FROM courses 
 WHERE course_num < 198 
 ORDER BY q_year DESC, q_rank DESC;
+"""
+
+# You can add more queries here as you learn!
+GET_EASIEST_CLASSES = """
+SELECT course, AVG(avgGPA) as mean_gpa
+FROM courses
+GROUP BY course
+ORDER BY mean_gpa DESC
+LIMIT 10;
 """
