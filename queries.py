5e84dd0731936bbf90be4cd5c9fd94f450d072c3
Joshua Chung <152940247+josh11100@users.noreply.github.com>
2026-02-23 12:59:14 -0800

    Update queries.py

--- queries.py
+++ queries.py
@@ -1,19 +1,18 @@
 # queries.py
 
 # Query to get standard undergraduate classes sorted by newest year first
-# We use DESC (Descending) to put 2024 above 2009
 GET_RECENT_LECTURES = """
-    SELECT * FROM df 
-    WHERE course_num < 198 
-    ORDER BY q_year DESC, q_rank DESC
+SELECT * FROM df 
+WHERE course_num < 198 
+ORDER BY q_year DESC, q_rank DESC
 """
 
-# Example of another query you could use later:
+# Example for later
 GET_TOP_PROFS = """
-    SELECT instructor, AVG(avgGPA) as mean_gpa
-    FROM df
-    GROUP BY instructor
-    HAVING COUNT(*) > 5
-    ORDER BY mean_gpa DESC
-    LIMIT 10
+SELECT instructor, AVG(avgGPA) as mean_gpa
+FROM df
+GROUP BY instructor
+HAVING COUNT(*) > 5
+ORDER BY mean_gpa DESC
+LIMIT 10
 """
